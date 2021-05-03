7f6ad2fb0a7327420b09a008cd357cfd9f89b632
abdullah-server <abdullahsaei@hotmail.com>
2021-05-03 19:25:35 +0200

    SR - poison scanner: add run.sh

---
+++ Sweet_Revange/poison_scanner/run.sh
@@ -0,0 +1,8 @@
+cd TE/Rooms/Sweet_Revenge/poison_scanner
+
+# for smooth transition instead of terminal appearance
+sudo fbi -a -T 1 --noverbose analyse.gif &
+
+# python script 
+python3 poison_scanner.py
+
